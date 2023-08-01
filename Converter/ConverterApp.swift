32d911060d87bfdaafec41e5f0dec653b043c912
Bradlee King <Kingofbrad@gmail.com>
2023-08-01 19:59:19 +0100

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Bradlee King on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
